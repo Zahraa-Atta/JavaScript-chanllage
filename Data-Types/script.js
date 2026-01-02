54784f2b5c5dac960162bac6f95833ecbb29831c
Zahraa Atta <zahraaatta5@gmail.com>
2026-01-03 00:40:10 +0300

    Add new folder

---
+++ Data-Types/script.js
@@ -0,0 +1,9 @@
+let population = 46;
+let country = "iraq";
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof population);
